2960005e3e02f5a490a0543ee8230da20a112bdf
Rithvik Pallamreddy <1017rithvik@gmail.com>
2025-08-02 18:00:44 -0500

    frontend initial commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
