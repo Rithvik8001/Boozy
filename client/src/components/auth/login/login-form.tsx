49dc9652589f1b09675d93e1ceb8e0e1e69f39f3
Rithvik Pallamreddy <1017rithvik@gmail.com>
2025-08-03 19:04:22 -0500

    login logic completed

--- client/src/components/auth/login/login-form.tsx
+++ client/src/components/auth/login/login-form.tsx
@@ -3,18 +3,48 @@ import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
-export default function LoginForm() {
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+interface LoginFormProps {
+  isLoading: boolean;
+  formData: LoginFormData;
+  setFormData: (formData: LoginFormData) => void;
+  handleLogin: () => void;
+}
+
+export default function LoginForm({
+  isLoading,
+  formData,
+  setFormData,
+  handleLogin,
+}: LoginFormProps) {
   return (
     <div className="mt-2 flex w-full flex-col gap-2">
       <div className="flex flex-col gap-2">
         <Label>Email</Label>
-        <Input type="email" placeholder="Email" />
+        <Input
+          type="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <Label>Password</Label>
-        <Input type="password" placeholder="Password" />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
       </div>
-      <Button>Login</Button>
+      <Button onClick={handleLogin} disabled={isLoading}>
+        {isLoading ? "Logging in..." : "Login"}
+      </Button>
       <Link
         to="/signup"
         className="text-muted-foreground text-sm hover:underline"
--- client/src/components/auth/login/login.tsx
+++ client/src/components/auth/login/login.tsx
@@ -1,8 +1,58 @@
+import axios, { AxiosError } from "axios";
 import LoginForm from "./login-form";
 import { ArrowLeft } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+interface LoginFormData {
+  email: string;
+  password: string;
+}
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [formData, setFormData] = useState<LoginFormData>({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    setIsLoading(true);
+    if (formData.email === "" || formData.password === "") {
+      toast.error("Please fill in all fields");
+      return;
+    } else if (!formData.email.includes("@")) {
+      toast.error("Please enter a valid email");
+      return;
+    } else if (formData.password.length < 8) {
+      toast.error("Password must be at least 8 characters long");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_BACKEND_BASE_URL + "/api/auth/login",
+        formData,
+      );
+      if (response.status === 200) {
+        toast.success("Login successful, redirecting to dashboard...", {
+          duration: 1000,
+        });
+        navigate("/dashboard");
+      } else {
+        toast.error("Invalid credentials, please try again");
+      }
+    } catch (error) {
+      if ((error as AxiosError).response?.status === 401) {
+        toast.error("Invalid credentials, please try again");
+      } else {
+        toast.error("Something went wrong, please try again");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="h-screen bg-gray-50">
@@ -23,7 +73,12 @@ export default function Login() {
                 <p className="text-muted-foreground text-md">
                   Login to your account to get started.
                 </p>
-                <LoginForm />
+                <LoginForm
+                  isLoading={isLoading}
+                  formData={formData}
+                  setFormData={setFormData}
+                  handleLogin={handleLogin}
+                />
               </div>
             </div>
           </div>
